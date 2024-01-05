5665dbcb4623e7a86fb9f43d343821d1a07ad30a
seokjaegon <seokjaegon@naver.com>
2024-01-05 15:57:41 +0900

    회원 게시판 테이블

--- dbclass.sql
+++ dbclass.sql
@@ -768,9 +768,90 @@ alter table student change s_mobile s_phone varchar(30);
 -- 컬럼 삭제
 alter table student drop s_major;
 
+drop table if exists member_table;
+create table member_table(
+	id bigint primary key auto_increment,
+    member_email varchar(30) not null unique,
+    member_name varchar(20) not null,
+    member_password varchar(20) not null
+);
 
+drop table if exists category_table;
+create table category_table(
+	id bigint primary key auto_increment,
+    category_name varchar(20) not null unique
+);
 
+drop table if exists board_table;
+create table board_table(
+	id bigint primary key auto_increment,
+    board_title varchar(50) not null,
+    board_writer varchar(30) not null,
+    board_contents varchar(500),
+    board_hits int default 0,
+    board_created_at datetime default now(),
+    board_updated_at datetime on update now(),
+    board_file_attached int default 0, -- 파일 첨부 여부(없으면 0, 있으면 1)
+    member_id bigint,
+    category_id bigint,
+    constraint fk_board_mid foreign key (member_id) references member_table(id) on delete cascade,
+    constraint fk_board_cid foreign key (category_id) references category_table(id) on delete set null
+);
 
+drop table if exists board_file_table;
+create table board_file_table(
+	id bigint primary key auto_increment,
+    original_file_name varchar(100), -- 사용자가 업로드한 파일의 이름
+    stored_file_name varchar(100),	-- 관리용 파일 이름(파일이름 생성 로직은 backend에서)
+    -- 증명사진.jpg 4352342523432-증명사진.jpg
+    board_id bigint,
+    constraint fk_bft_bid foreign key (board_id) references board_table(id) on delete cascade
+);
+
+drop table if exists comment_table;
+create table comment_table(
+	id bigint primary key auto_increment,
+    comment_writer varchar(30) not null,
+    comment_contents varchar(200) not null,
+    comment_created_at datetime default now(),
+    board_id bigint,
+    member_id bigint,
+    constraint fk_ct_bid foreign key (board_id) references board_table(id) on delete cascade,
+    constraint fk_ct_mid foreign key (member_id) references member_table(id) on delete cascade
+);
+
+drop table if exists good_table;
+create table good_table(
+	id bigint primary key auto_increment,
+    comment_id bigint,
+    member_id bigint,
+    constraint fk_gt_cid foreign key (comment_id) references comment_table(id) on delete cascade,
+    constraint fk_gt_mid foreign key (member_id) references member_table(id) on delete cascade
+);
+
+-- 회원 기능
+-- 1. 회원가입(임의의 회원3명 가입)
+insert into member_table(member_email, member_name, member_password) values('aa@aa.com','aa','1111');
+insert into member_table(member_email, member_name, member_password) values('bb@bb.com','bb','2222');
+insert into member_table(member_email, member_name, member_password) values('cc@cc.com','cc','3333');
+-- 2. 이메일 중복체크 
+select member_email from member_table where member_email = 'aa@aa.com';
+-- 3. 로그인
+select member_email, member_password from member_table 
+	where member_email = 'aa@aa.com' and member_password = '1111';
+-- 4. 전체 회원 목록 조회 
+select * from member_table;
+-- 5. 특정 회원만 조회 
+select * from member_table where member_email = 'aa@aa.com';
+select * from member_table where id = 1;
+-- 6. 내정보 수정하기(6.1, 6.2에 해당하는 쿼리문작성)
+-- 6.1 회원정보 수정화면 요청(회원정보 수정 페이지를 보여준다고 가정했을 때 필요한 쿼리) 
+select * from member_table where member_email = 'aa@aa.com';
+select * from member_table where id = 1;
+-- 6.2 회원정보 수정 처리(비밀번호를 변경한다는 상황)
+update member_table set member_password = '2222' where id = 1;
+-- 7. 회원 삭제 또는 탈퇴 
+delete from member_table where id = 1;
 
 
 
